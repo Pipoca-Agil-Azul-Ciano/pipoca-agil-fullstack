ff4d794f56c9f951bca14e655f37bdc0339e9f40
Diego B <dbsmendes@gmail.com>
2023-11-19 14:01:53 -0300

    Feat: Implemented Swagger Documentation

--- backend/src/main/java/br/com/pipoca/PipocaAgilBackend/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/br/com/pipoca/PipocaAgilBackend/config/swagger/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package br.com.pipoca.PipocaAgilBackend.config.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pipoca Ágil API")
+                        .description("SpringBoot Application")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Azul Ciano")
+                                .email("")
+                                .url("https://github.com/Pipoca-Agil-Azul-Ciano/pipoca-agil-fullstack")));
+    }
 }
