41de9b017a1c62dfc9053b02444bae0511ce369a
Diego B <dbsmendes@gmail.com>
2023-11-20 22:28:07 -0300

    Feat: Removed Password validation

--- backend/src/main/java/br/com/pipoca/PipocaAgilBackend/config/Constantes.java
+++
@@ -1,5 +0,0 @@
-package br.com.pipoca.PipocaAgilBackend.config;
-
-public class Constantes {
-    public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W])[\\w\\W]{8,}$";
-}
