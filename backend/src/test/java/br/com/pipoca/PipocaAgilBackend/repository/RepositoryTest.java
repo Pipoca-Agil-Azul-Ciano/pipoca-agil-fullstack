fd585d07c0bb97d8c93552b6a9d349305a6a32e3
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-11-28 02:32:33 +0000

    Fix: "Add validation to UserRegisterDTO, update RepositoryTest, and miscellaneous changes"

--- backend/src/test/java/br/com/pipoca/PipocaAgilBackend/repository/RepositoryTest.java
+++ backend/src/test/java/br/com/pipoca/PipocaAgilBackend/repository/RepositoryTest.java
@@ -1,11 +1,14 @@
 package br.com.pipoca.PipocaAgilBackend.repository;
 
 import br.com.pipoca.PipocaAgilBackend.entity.User;
+import br.com.pipoca.PipocaAgilBackend.enums.UserTypeEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -28,11 +31,17 @@ public class RepositoryTest {
         updatedUser.setId(1);
         updatedUser.setFullName("Novo Nome");
         updatedUser.setEmail("novonome@example.com");
+        updatedUser.setPassword("Senha123@Teste");
+        updatedUser.setUserTypeEnum(UserTypeEnum.SUBSCRIBE);
+        updatedUser.setDateBirth(LocalDate.ofEpochDay(2000-10-30));
 
         User existingUser = new User();
         existingUser.setId(1);
         existingUser.setFullName("Nome Antigo");
         existingUser.setEmail("nomeantigo@example.com");
+        updatedUser.setPassword("Senha123@Teste2");
+        updatedUser.setUserTypeEnum(UserTypeEnum.REGISTERED);
+        updatedUser.setDateBirth(LocalDate.ofEpochDay(1989-10-30));
 
         when(userRepository.findById(updatedUser.getId())).thenReturn(existingUser);
 
@@ -44,5 +53,8 @@ public class RepositoryTest {
 
         assertEquals(updatedUser.getFullName(), existingUser.getFullName());
         assertEquals(updatedUser.getEmail(), existingUser.getEmail());
+        assertEquals(updatedUser.getPassword(), existingUser.getPassword());
+        assertEquals(updatedUser.getUserTypeEnum(), existingUser.getUserTypeEnum());
+        assertEquals(updatedUser.getDateBirth(), existingUser.getDateBirth());
     }
 }
