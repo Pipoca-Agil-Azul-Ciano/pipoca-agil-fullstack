8c130b792ee84db5d5162c775586a324801a3ce6
FIXER3600 <guilhermecomercial51@gmail.com>
2024-01-04 17:17:46 -0300

    feat:mensagens de erro para os campos de senha e email para recuperação de senha

--- frontend/src/components/TextField.jsx
+++ frontend/src/components/TextField.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Center, Input } from "@chakra-ui/react";
 
       
-function TextField({ field, hasError, ...props } ) {
+function TextField({ field, hasError,isCheck, ...props } ) {
   return (
     <Center>
     
@@ -10,13 +10,13 @@ function TextField({ field, hasError, ...props } ) {
      
       {...field} {...props}
 
-        focusBorderColor={hasError ? "red.500" : "#866FAD"}
+        focusBorderColor={hasError ? "red.500" : isCheck? '#2B6CB0':"#575450"}
         width={"30.5em"}
         placeholdersize="md"
-        borderColor={"#575450"}
+        borderColor={ hasError ? "red.500" : isCheck? '#2B6CB0':"#575450"}
         height={"64px"}
         borderRadius={10}
-       
+
       />
       
     </Center>
