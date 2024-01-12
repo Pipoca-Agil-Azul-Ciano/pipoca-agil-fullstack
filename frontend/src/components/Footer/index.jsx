e3463e1e138e39519c9670acdfe2913e6f362379
FIXER3600 <guilhermecomercial51@gmail.com>
2024-01-12 18:03:12 -0300

    feat: Logos de redes sociais

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,6 +1,12 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import Logo from "../../assets/Dashboard/logo-footer.png";
+import Logo from "../../assets/Dashboard/Footer/logo-footer.png";
+import Youtube from '../../assets/Dashboard/Footer/YouTube.png'
+import Facebook from '../../assets/Dashboard/Footer/Facebook.png'
+import Linkedin from "../../assets/Dashboard/Footer/LinkedIn.png"
+import X from "../../assets/Dashboard/Footer/TwitterX.png"
+import Spotify from "../../assets/Dashboard/Footer/Spotify.png";
+import Instagram from "../../assets/Dashboard/Footer/Instagram.png"
 export default function Footer() {
   return (
     <Box
@@ -8,6 +14,8 @@ export default function Footer() {
       height={"216px"}
       width={"100%"}
       backgroundColor={"#585858"}
+      justifyContent={'space-around'}
+      alignItems={'center'}
     >
       <Image src={Logo} height={"70px"} width={"90px"} />
       <Box display={"flex"} color={'#FFF'} flexDirection={'column'}>
@@ -20,12 +28,24 @@ export default function Footer() {
         <Text>Comunidade</Text>
         <Text>Políticas de privacidade</Text>
         <Text>Contate-nos</Text>
+	
       </Box>
-      <Box>
-        <Text></Text>
+      <Box display={"flex"} color={'#FFF'} flexDirection={'column'}>
+        <Text marginBottom={'10px'} marginLeft={'7px'}>Siga-nos nas redes sociais</Text>
+	<Box display={"flex"} >
+	<Image src={Instagram} padding={'5px'} height={"61px"} width={"48px"} />
+	<Image src={Linkedin}  padding={'5px'}height={"61px"} width={"48px"} />
+	<Image src={Facebook}  padding={'5px'}height={"61px"} width={"48px"} />
+	<Image src={X} padding={'5px'} height={"61px"} width={"48px"} />
+	</Box>
       </Box>
-      <Box>
-        <Text></Text>
+      <Box display={"flex"} color={'#FFF'} flexDirection={'column'}>
+        <Text>Ouça-nos também</Text>
+	<Box display={"flex"}>
+	<Image src={Spotify} padding={'5px'} height={"61px"} width={"48px"} />
+	<Image src={Youtube} padding={'5px'} height={"61px"} width={"48px"} />
+
+	</Box>
       </Box>
     </Box>
   );
